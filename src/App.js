51f041eb1a1ac91119a50866637da17549b3e3e4
vadimgaidai <vadim.gaidai@icloud.com>
2018-07-07 17:10:21 +0300

    components header, link, menu

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 import Grid from 'react-bootstrap/lib/Grid';
 import Row from 'react-bootstrap/lib/Row';
@@ -7,42 +7,57 @@ import Col from 'react-bootstrap/lib/Col';
 
 import styled from 'styled-components';
 
+
+import Header from './components/header'
+import Menu from './components/menu'
+import Main from './components/main'
+
+const bgMain = process.env.PUBLIC_URL + './img/bg.jpg';
+
+const HeaderWrapper = styled.header`
+    width: 100%;
+    height: 49px;
+    background-color: #242424;
+    opacity: 0.8;
+`
+const MenuWrapper = styled.div`
+    height: 89px;
+    padding-top: 20px;
+`
+
+const MainWrapper = styled.main`
+    width: 100%;
+    min-height: 600px;
+    padding-top: 130px;
+    background: url(${bgMain}) no-repeat;
+    background-size: cover;
+`
 class App extends Component {
   render() {
     return (
-      /*<div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>*/
 
+        <div className="App">
+            <HeaderWrapper>
+                <Grid>
+                    <Header></Header>
+                </Grid>
+            </HeaderWrapper>
+            <MenuWrapper>
+                <Grid>
+                    <Menu></Menu>
+                </Grid>
+            </MenuWrapper>
+            <MainWrapper>
+                <Grid>
+                    <Main></Main>
+                </Grid>
+            </MainWrapper>
 
-        <div className="Wrapper">
-            <SayFullName name ="Вадим" surname="Гайдай" link="vk.com" />
-            
         </div>
-
 
     );
   }
 }
-
-function SayFullName(props) {
-
-    return(
-        <div>
-            <h1>Мое имя - {props.name}, фамилия - {props.surname}</h1>
-            <a href={props.link}>Ссылка на мой профиль </a>
-        </div>
-    )
-    
-}
-
-
 
 
 export default App;
---
+++ src/components/main.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+
+
+
+import Row from 'react-bootstrap/lib/Row';
+import Col from 'react-bootstrap/lib/Col';
+
+import styled from 'styled-components';
+
+
+class  Main extends React.Component{
+
+    render(){
+        return(
+            <Row>
+                <Col lg={5}>
+
+                </Col>
+
+                <Col lg={6} lgOffset={1}>
+
+                </Col>
+
+            </Row>
+        );
+    }
+
+}
+
+
+export  default Main
